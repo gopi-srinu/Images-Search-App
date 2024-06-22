ba109ed33a7f434caf13f5a117df8ff430acfda2
Gopi Srinivas Pydipati <142145103+gopi-srinu@users.noreply.github.com>
2024-06-22 21:33:30 +0530

    Update image.service.ts

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -8,7 +8,7 @@ import { Imageresponse } from '../Model/image';
 })
 export class ImageService {
   constructor(private http: HttpClient) {}
-  private apiKey = '34717032-375c5ce9913202eb4906f0f1a';
+  private apiKey = '';
   getImages(searchQuery: string): Observable<Imageresponse>{
     const searchURL = `https://pixabay.com/api/?key=${this.apiKey}&q=${searchQuery}&image_type=photo&pretty=true`;
     return this.http.get<Imageresponse>(searchURL);
